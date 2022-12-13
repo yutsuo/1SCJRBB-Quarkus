9ee088d7ce6972d40a9723e0445f786373f17609
Yutsuo <yutsuo@gmail.com>
2022-12-13 09:18:39 -0300

    Last touches

--- build-and-run.ps1
+++ build-and-run.ps1
@@ -1,3 +1,3 @@
 quarkus build
 docker build . -f .\src\main\docker\Dockerfile.jvm -t fiap/quarkus-joinha:1.0.0
-docker compose up
+docker-compose up -d
--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 quarkus build &&
 docker build . -f ./src/main/docker/Dockerfile.jvm -t fiap/quarkus-joinha:1.0.0 &&
-docker compose up
+docker-compose up -d
